e88e94131f173679e8330405aae839d161d46b75
Laxmi Kandel <laxmikandel97@gmail.com>
2020-01-15 14:29:20 -0500

    Added config file and index page

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 /**
  * Laxmi Kandel
+ * 01/15/2020
+ * /IT328/chicken/index.php
  */
 //TURN ON ERROR REPORTING
 
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
+
+//session start() wont work if there is space or any echo statement after php or before php
+session_start();
