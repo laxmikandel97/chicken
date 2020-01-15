6b4b254fe7597dfc7de8b8d6020389bd84fde411
Laxmi Kandel <laxmikandel97@gmail.com>
2020-01-15 14:39:51 -0500

    set up default route

--- index.php
+++ index.php
@@ -10,4 +10,19 @@ ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
 //session start() wont work if there is space or any echo statement after php or before php
-session_start();
+//session_start();
+
+require "vendor/autoload.php";
+
+//Instantiate F3
+$f3 = Base::instance();
+//defining a default route
+//it means when the user navigate through root directory (chicken) they should see the output
+//we wont have index.php
+
+$f3->route('GET /',function ()
+{
+    echo "Hello";
+});
+//Run fat free
+$f3-> run();
